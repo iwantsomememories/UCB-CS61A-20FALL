3f34c7cf22e2f3f2be931c08cafd7ffa589946e4
fanqiechaodan <1484119655@qq.com>
2024-09-25 17:13:12 +0800

    Finish the part 3 of the project scheme

--- project/scheme/questions.scm
+++ project/scheme/questions.scm
@@ -6,14 +6,44 @@
 ; Some utility functions that you may find useful to implement
 
 (define (zip pairs)
-  'replace-this-line)
+  (define (get lst index)
+    (if (= index 0)
+      (car lst)
+      (get (cdr lst) (- index 1)))
+    )
+  (define (gen_i_item pairs index)
+    (if (eq? pairs nil)
+      nil
+      (cons (get (car pairs) index) 
+            (gen_i_item (cdr pairs) index))
+      )
+    )
+  (define x (length (car pairs)))
+  (define (func pairs len cur_len)
+    (if (= len cur_len)
+      (cons (gen_i_item pairs cur_len) 
+            nil)
+      (cons (gen_i_item pairs cur_len) 
+            (func pairs len (+ cur_len 1)))
+      )
+    )
+  (func pairs (- x 1) 0)
+ )
 
 
 ;; Problem 15
 ;; Returns a list of two-element lists
 (define (enumerate s)
   ; BEGIN PROBLEM 15
-  'replace-this-line
+   (define (helper s index)
+     (if (eq? s nil)
+         nil
+         (cons (cons index 
+                     (cons (car s) 
+                           nil)) 
+               (helper (cdr s) 
+                       (+ index 1)))))
+   (helper s 0)
   )
   ; END PROBLEM 15
 
@@ -23,7 +53,13 @@
 ;; the merged lists.
 (define (merge comp list1 list2)
   ; BEGIN PROBLEM 16
-  'replace-this-line
+  (cond
+    ((eq? list1 nil) list2)
+    ((eq? list2 nil) list1)
+    ((comp (car list1) (car list2)) 
+        (cons (car list1) (merge comp (cdr list1) list2)))
+    (else
+        (cons (car list2) (merge comp list1 (cdr list2)))))
   )
   ; END PROBLEM 16
 
@@ -37,8 +73,17 @@
 
 (define (nondecreaselist s)
     ; BEGIN PROBLEM 17
-    'replace-this-line
-    )
+    (define (helper s cur_list cur_item)
+      (cond 
+        ((eq? s nil)
+            (cons cur_list nil))
+        ((or (eq? cur_list nil) (<= cur_item (car s)))
+            (helper (cdr s) (append cur_list (cons (car s) nil)) (car s)))
+        (else (cons cur_list (helper s nil 0)))
+        )
+      )
+    (helper s nil 0)
+)
     ; END PROBLEM 17
 
 ;; Problem EC
@@ -55,12 +100,12 @@
 (define (let-to-lambda expr)
   (cond ((atom? expr)
          ; BEGIN PROBLEM EC
-         'replace-this-line
+         expr
          ; END PROBLEM EC
          )
         ((quoted? expr)
          ; BEGIN PROBLEM EC
-         'replace-this-line
+         expr
          ; END PROBLEM EC
          )
         ((or (lambda? expr)
@@ -69,19 +114,23 @@
                (params (cadr expr))
                (body   (cddr expr)))
            ; BEGIN PROBLEM EC
-           'replace-this-line
+           (cons form (cons params (map let-to-lambda body)))
            ; END PROBLEM EC
            ))
         ((let? expr)
          (let ((values (cadr expr))
                (body   (cddr expr)))
            ; BEGIN PROBLEM EC
-           'replace-this-line
+           (append (cons (cons 'lambda 
+                               (cons (car (zip values)) 
+                                     (map let-to-lambda body))) 
+                         nil) 
+                   (map let-to-lambda (cadr (zip values))))
            ; END PROBLEM EC
            ))
         (else
          ; BEGIN PROBLEM EC
-         'replace-this-line
+         (map let-to-lambda expr)
          ; END PROBLEM EC
          )))
 
